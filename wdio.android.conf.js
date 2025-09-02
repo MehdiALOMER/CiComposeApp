a2202241c89423ba48a1ade38e5a404967cb600e
mehdi <mehdi.alomer98@gmail.com>
2025-09-02 14:52:03 +0300

    test

--- wdio.android.conf.js
+++ wdio.android.conf.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 const appPath = process.env.ANDROID_APP_PATH || path.resolve(__dirname, 'app/build/outputs/apk/debug/app-debug.apk');
 
 exports.config = {
@@ -11,10 +10,12 @@ exports.config = {
   mochaOpts: { timeout: 180000 },
   reporters: ['spec'],
 
-  // Appium server'a bağlan (Appium 2 default: 127.0.0.1:4723)
   hostname: '127.0.0.1',
   port: 4723,
   path: '/',
+
+  // bağlantı/yeniden deneme toleransı
+  connectionRetryTimeout: 120000,
 
   capabilities: [
     {
@@ -23,6 +24,11 @@ exports.config = {
       'appium:app': appPath,
       'appium:autoGrantPermissions': true,
       'appium:newCommandTimeout': 180,
+      // --- stabilite ayarları ---
+      'appium:uiautomator2ServerLaunchTimeout': 120000, // uia2 server başlama süresi
+      'appium:adbExecTimeout': 200000,                   // adb ağır komutlar
+      'appium:disableWindowAnimation': true,             // yine animasyon kapansın
+      'appium:appWaitActivity': '*',                     // activity beklemesini gevşet
     },
   ],
 };
