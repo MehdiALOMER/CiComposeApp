369935a453472d6d12f10d2871e50c7f4abcc52d
mehdi <mehdi.alomer98@gmail.com>
2025-09-03 09:59:12 +0300

    test

--- wdio.android.conf.js
+++ wdio.android.conf.js
@@ -1,44 +1,34 @@
+// path: wdio.android.conf.js
+const path = require('path');
+const appPath = process.env.ANDROID_APP_PATH || path.resolve(__dirname, 'app/build/outputs/apk/debug/app-debug.apk');
 
-  const path = require('path');
-  const appPath = process.env.ANDROID_APP_PATH || path.resolve(__dirname, 'app/build/outputs/apk/debug/app-debug.apk');
+exports.config = {
+  runner: 'local',
+  specs: ['./e2e/**/*.spec.js'],
+  maxInstances: 1,
+  logLevel: 'info',
+  framework: 'mocha',
+  mochaOpts: { timeout: 180000 },
+  reporters: ['spec'],
 
-  exports.config = {
-runner: 'local',
-specs: ['./e2e/**/*.spec.js'],
-maxInstances: 1,
-logLevel: 'info',
-framework: 'mocha',
-mochaOpts: { timeout: 180000 },
-reporters: ['spec'],
+  hostname: '127.0.0.1',
+  port: 4723,
+  path: '/wd/hub',
 
-hostname: '127.0.0.1',
-port: 4723,
-path: '/',
+  connectionRetryTimeout: 120000,
 
-connectionRetryTimeout: 120000,
-
-  // Appium server'ı WDIO servis olarak başlatır
-services: [[
-  'appium',
-  {
-    command: 'appium',
-    args: { address: '127.0.0.1', port: 4723, basePath: '/', loglevel: 'warn' },
-    logPath: './appium-ci-logs',
-  },
-]],
-
-capabilities: [
-  {
-    platformName: 'Android',
-    'appium:automationName': 'UiAutomator2',
-    'appium:app': appPath,
-    'appium:autoGrantPermissions': true,
-    'appium:newCommandTimeout': 180,
-    // stabilite
-    'appium:uiautomator2ServerLaunchTimeout': 120000,
-    'appium:adbExecTimeout': 200000,
-    'appium:disableWindowAnimation': true,
-    'appium:appWaitActivity': '*',
-  },
-],
+  capabilities: [
+    {
+      platformName: 'Android',
+      'appium:automationName': 'UiAutomator2',
+      'appium:app': appPath,
+      'appium:autoGrantPermissions': true,
+      'appium:newCommandTimeout': 180,
+      // stability
+      'appium:uiautomator2ServerLaunchTimeout': 120000,
+      'appium:adbExecTimeout': 200000,
+      'appium:disableWindowAnimation': true,
+      'appium:appWaitActivity': '*',
+    },
+  ],
 };
